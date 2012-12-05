7063ecb46e53b68141b21c16ddc306d3ed14f53e
James Maniscalco <james@James-MBP.local>
2012-12-05 14:35:56 -0500

    moved Mustache templates to external file

--- chicago/gear/templatetags/template_filters.py
+++ chicago/gear/templatetags/template_filters.py
@@ -1,4 +1,4 @@
-# template filters for Gear app
+# template filters and tags for Gear app
 
 from django import template
 
@@ -9,3 +9,47 @@ register = template.Library()
 @register.filter
 def gear_item_weight(gear_item, user):
     return gear_item.get_weight(user)
+
+
+"""
+From Gist 1313862:
+
+jQuery templates use constructs like:
+
+    {{if condition}} print something{{/if}}
+
+This, of course, completely screws up Django templates,
+because Django thinks {{ and }} mean something.
+
+Wrap {% verbatim %} and {% endverbatim %} around those
+blocks of jQuery templates and this will try its best
+to output the contents with no changes.
+"""
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+    
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
